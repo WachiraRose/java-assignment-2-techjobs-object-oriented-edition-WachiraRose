51dc83227381651b5254e7ed7ed7e2437fbe1e88
Rose Wachira <laurose20@gmail.com>
2023-05-24 20:17:00 -0500

    Tasktwo tests all Passed

--- src/main/java/org/launchcode/techjobs/oo/CoreCompetency.java
+++ src/main/java/org/launchcode/techjobs/oo/CoreCompetency.java
@@ -40,5 +40,16 @@ public class CoreCompetency {
 
     // TODO: Use the "Generate" tool to add a getter and setter for the 'value' field but
     //  ONLY a getter for the 'id' field.
+// getter and setter generated for value
+    public String getValue() {
+        return value;
+    }
 
+    public void setValue(String value) {
+        this.value = value;
+    }
+// generated get value as their is a unique id already set in the constructor
+    public int getId() {
+        return id;
+    }
 }
--- src/main/java/org/launchcode/techjobs/oo/Location.java
+++ src/main/java/org/launchcode/techjobs/oo/Location.java
@@ -16,6 +16,12 @@ public class Location {
     // TODO: Add a constructor that takes a string as a parameter and assigns it to the 'value' field. The
     //  constructor should also call the empty constructor in order to initialize the 'id' field.
 
+    //constructor
+    public Location(String value) {
+        this();// empty constructor
+        this.value = value;
+    }
+
 
     // Custom toString, equals, and hashCode methods:
 
